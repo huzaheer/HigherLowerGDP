c34d2ff48423d7806a94ea65c83ab4d32ddde226
Humza Chouhdry <hc345@duke.edu>
2023-09-30 17:02:42 -0400

    working scraping

--- scraping.py
+++ scraping.py
@@ -5,10 +5,13 @@ with open('GDP.html', 'r') as html_file:
 
     soup = BeautifulSoup(content, 'lxml')
     tags_row = soup.find_all('tr')
-
+    i = 0
     for row in tags_row:
         td_elements = row.find_all('td')  # Find all <td> elements within the current <tr>
         
         row_entry = [td_element.text for td_element in td_elements]
-        print(row_entry)
+        i +=1
+        if i > 2:
+            row_entry
+            print(f"i: {i}, row_entry: {row_entry[0]}")
 
