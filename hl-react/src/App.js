0956a0963c35e1a27fb803a1db3ac9acf8b27c34
Humza Chouhdry <hc345@duke.edu>
2023-10-17 22:10:16 -0400

    somewhat lost

--- hl-react/src/App.js
+++ hl-react/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We will create a game, god help us
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          fuck arvindh
-        </a>
-      </header>
+    <div className="Cta" style={{width: 1440, height: 1410, position: 'relative', background: 'white'}}>
+      <div className="HeroSideImageRight" style={{width: 1440, height: 700, left: 0, top: 710, position: 'absolute'}}>
+        <div className="Background" style={{width: 1440, height: 700, left: 0, top: 0, position: 'absolute', background: '#F1F2FE'}} />
+        <div className="Content" style={{width: 399, height: 197, left: 259, top: 220, position: 'absolute'}}>
+          <div className="Title" style={{width: 399, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 56, fontFamily: 'Roboto', fontWeight: '500', wordWrap: 'break-word'}}>Let’s test your geopolitical knowledge</div>
+        </div>
+      </div>
+      <button className="ButtonPrimary" style={{width: 270, height: 110, left: 868, top: 891, position: 'absolute', background: '#832BAB', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 20, overflow: 'hidden'}}>
+        <div className="Background" style={{width: 270, height: 110, left: 0, top: 0, position: 'absolute', background: '#3E1452', borderRadius: 5}} />
+        <div className="Text" style={{width: 221, left: 25, top: 42, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Ropa Sans', fontStyle: 'italic', fontWeight: '400', wordWrap: 'break-word'}}>By GDP</div>
+      </button>
+      <div className="ButtonPrimary" style={{width: 265, height: 110, left: 870, top: 1069, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 20, overflow: 'hidden'}}>
+        <div className="Background" style={{width: 265, height: 110, left: 0, top: 0, position: 'absolute', background: '#EDB6C7', borderRadius: 5}} />
+        <div className="Text" style={{width: 216, left: 25, top: 42, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Ropa Sans', fontStyle: 'italic', fontWeight: '400', wordWrap: 'break-word'}}>By GDP Per Capita</div>
+      </div>
+      <div className="Frame1" style={{width: 1440, height: 710, left: 0, top: 0, position: 'absolute'}} />
     </div>
   );
 }
